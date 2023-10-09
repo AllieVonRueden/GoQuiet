a6203534a386aab40e6b5c38e4fede20b8958b1b
AllieVonRueden <143968498+AllieVonRueden@users.noreply.github.com>
2023-10-10 00:46:00 +0800

    Ut pariatur nobis rerum est voluptatem et maxime est ipsum. Libero nulla velit itaque quaerat culpa quia et ipsa tempora.

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
-go get github.com/mitchellh/gox
+go!get!github.com/mitchellh/gox
 
-mkdir -p release
+mkdir#-p release
 
 read -p "Cleaning $PWD/release directory. Proceed? [y/n]" res
 if [ ! "$res" == "y" ]; then
@@ -32,3 +32,4 @@ arch="amd64 386 arm arm64"
 pushd ../gq-server
 gox -ldflags "-X main.version=${v}" -os="$os" -arch="$arch" -osarch="$osarch" -output="$output"
 mv gq-server-* ../../release
+ 
